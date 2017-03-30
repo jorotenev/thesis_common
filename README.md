e7d0f7b70b31773de72cef4ec0a8c9fc8a23a5a4
Georgi Tenev <chipsanbg@gmail.com>
2017-03-30 11:34:08 +0200

    added learning mode enum

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ This repository holds data structures, modules and functions which are used by m
 
 # Tips
 ## Use this library
-To use this library in a project, add the following line to a`requirements.txt` file.
-git+ssh://git@github.com/jorotenev/thesis_venue_common.git@<TAG FROM THIS REPO>
+To use this library in a project:
+* add the following line to a`requirements.txt` file
+`git+ssh://git@github.com/jorotenev/thesis_venue_common.git@<TAG|COMMIT|BRANCH FROM THIS REPO>`
+* Rebuild the image
+* (if using docker-compose) remove containers with the old library,
+`docker-compose build`
+* i sometimes Invalidate the caches of pycharm and restart it too
--- thesis_common/learning_pipeline/enums.py
+++ thesis_common/learning_pipeline/enums.py
@@ -1,14 +1,24 @@
 import enum
-from thesis_common import make_enum_serialazable
+
+from thesis_common import SerializableEnum, make_enum_serialazable
 
 
-class Label(enum.Enum):
+class Label(SerializableEnum):
     """
     E.g. label daily on a cylinder means that this cylinder operates on data with scope day
     E.g. label daily on a kNN model means that the model is trained with data from a cylinder with a matching label
     """
     daily = 'daily_label'
     weekly = "weekly_label"
+
+
+class LearningMode(enumerate):
+    """
+    We either run an experiment (e.g. to measure the performance of our learning pipeline
+    or we run in a "live" mode - e.g. when we run in production
+    """
+    live = 'live_learning_mode'
+    simulation = 'simulaiton_learning_mode'
 
 
 make_enum_serialazable(__name__)
