dd0d103111cad47011b423909af18b9382cbeccd
Georgi Tenev <chipsanbg@gmail.com>
2017-03-30 09:27:04 +0200

    added venue capacity and names to the Detached object

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -1,7 +1,6 @@
 from thesis_common.learning_pipeline import VenueMeasurementDetached
 from thesis_common import CustomJsonEncoder, deserialize_obj_hook
 
-
 from unittest import TestCase
 from datetime import datetime as dt
 import json
@@ -10,10 +9,17 @@ import json
 class TestVenueMeasurementDetached(TestCase):
     def test_obj_serialize(self):
         num_people = 10
+        capacity = 100
+        venue_name = "Gym1"
         now = dt.now()
-        obj = VenueMeasurementDetached(number_of_people=num_people, timestamp_local=now)
+        obj = VenueMeasurementDetached(number_of_people=num_people,
+                                       timestamp_local=now,
+                                       venue_name=venue_name,
+                                       venue_capacity=capacity)
         json_str = json.dumps(obj, cls=CustomJsonEncoder)
 
         deserialized = json.loads(json_str, object_hook=deserialize_obj_hook)
         self.assertEqual(num_people, deserialized.number_of_people)
         self.assertEqual(now, deserialized.timestamp_local)
+        self.assertEqual(venue_name, deserialized.venue_name)
+        self.assertEqual(capacity, deserialized.venue_capacity)
--- thesis_common/learning_pipeline/venue_measurement_detached.py
+++ thesis_common/learning_pipeline/venue_measurement_detached.py
@@ -2,16 +2,22 @@ from thesis_common.common import make_class_serializable
 
 
 class VenueMeasurementDetached(object):
-    def __init__(self, number_of_people, timestamp_local):
+    def __init__(self, number_of_people, timestamp_local, venue_capacity, venue_name):
         """
 
         :param number_of_people:
-         :type int
+        :type int
         :param timestamp_local: localized datetime (i.e. not UTC)
-         :type datetime.datetime
+        :type datetime.datetime
+        :param venue_name: the name of the venue (can be used to uniquely identify a venue in the Historical DB.
+        :type str
+        :param venue_capacity - how many people the venue can fit
+        :type int
         """
         self.number_of_people = number_of_people
         self.timestamp_local = timestamp_local
+        self.venue_capacity = venue_capacity
+        self.venue_name = venue_name
 
 
 make_class_serializable(VenueMeasurementDetached)
