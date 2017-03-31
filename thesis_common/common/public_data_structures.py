7783133cbac626e3d5e45ae9cd7c581ed589220d
Georgi Tenev <chipsanbg@gmail.com>
2017-03-31 16:23:22 +0200

    added local time in venue info

--- thesis_common/common/public_data_structures.py
+++ thesis_common/common/public_data_structures.py
@@ -6,9 +6,16 @@ class VenueInformation(object):
     Brief information about the data the Historical API has about a venue
     """
 
-    def __init__(self, oldest_measurement_dt_utc, newest_measurement_dt_utc, number_of_entries):
+    def __init__(self, oldest_measurement_dt_utc,
+                 oldest_measurement_dt_local,
+                 newest_measurement_dt_utc,
+                 newest_measurement_dt_local,
+                 number_of_entries):
         self.oldest_measurement_dt_utc = oldest_measurement_dt_utc
+        self.oldest_measurement_dt_local = oldest_measurement_dt_local
         self.newest_measurement_dt_utc = newest_measurement_dt_utc
+        self.newest_measurement_dt_local = newest_measurement_dt_local
+
         self.number_of_entries = number_of_entries
 
 
